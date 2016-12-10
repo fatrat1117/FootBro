afad635b960227da96810282534322beb15e6afa
ljx213101212 <lijixiang.nus@gmail.com>
2016-12-10 14:32:58 +0800

    dummy main page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {TransPipe} from '../../providers/localization'
+import {Localization} from '../../providers/localization';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,38 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  slides: any[];
+  adSlideOptions: any;
 
+  constructor(public navCtrl: NavController, local: Localization) {
+
+    this.slides = [];
+    this.loadSlides(local.langCode, 4);
+
+    this.adSlideOptions = {
+      autoplay: 3000,
+      loop: true
+    };
+    console.log(local.langCode);
+
+
+  }
+
+  loadSlides(langCode: string, total: number) {
+    let path = `assets/img/banners/${langCode}/`
+    for (let i = 0; i < total; ++i) {
+      this.slides.push({
+        image: `assets/img/banners/${langCode}/${i}.jpg`
+      });
+    }
+  }
+
+  goLeaguePage() {
+    return;
+  }
+
+  enterStandings() {
+    return;
   }
 
 }
