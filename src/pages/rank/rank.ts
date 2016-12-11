41c937cd639d92136c0bb0e39c274297c03e36df
fatrat1117 <fatrat1117@gmail.com>
2016-12-11 18:00:45 +0800

    sort

--- src/pages/rank/rank.ts
+++ src/pages/rank/rank.ts
@@ -20,11 +20,18 @@ export class RankPage {
     this.rankService.getTeamRanks().then(teamRanks => {
       //console.log(teamRanks);
       this.teamRanks = teamRanks;
+      this.sortTeamBy(this.teamSortByStr);
     });
   }
 
   sortTeamBy(str) {
     this.teamSortByStr = str;
+    this.teamRanks.sort((a, b)=> {
+      if ("ability" == this.teamSortByStr)
+        return b.ability - a.ability;
+      else 
+        return b.popularity - a.popularity;
+    });
     //this.teamSortBy.next(this.teamSortByStr);
   }
 
@@ -33,6 +40,7 @@ export class RankPage {
     setTimeout(() => {
       this.rankService.getMoreTeamRanks().then(teamRanks => {
         //console.log(teamRanks);  
+        //this.sortTeamBy(this.teamSortByStr);
         infiniteScroll.complete();
       });
     }, 500);
