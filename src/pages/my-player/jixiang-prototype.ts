98b014897f53990d4be06dc5cecb762fdae82872
ljx213101212 <lijixiang.nus@gmail.com>
2016-12-17 23:02:55 +0800

    add progress bar demo

--- src/pages/my-player/jixiang-prototype.ts
+++ src/pages/my-player/jixiang-prototype.ts
@@ -9,7 +9,23 @@ import {NavController} from 'ionic-angular';
 
 export class JixiangPrototypePage {
 
+  NumOfLikes : number;
+  NumOfUnLikes : number;
+  PercentOfLikes : number;
+  PercentOfUnLikes : number;
+
   constructor(private nav: NavController) {
+
+    var fromDBLikes = 1390;
+    var fromDBUnLikes = 60;
+
+
+    this.NumOfLikes = fromDBLikes;
+    this.NumOfUnLikes = fromDBUnLikes;
+    this.PercentOfLikes = fromDBLikes/(fromDBLikes+fromDBUnLikes) * 100;
+    this.PercentOfUnLikes = fromDBUnLikes/ (fromDBLikes+fromDBUnLikes) * 100;
+
+
 
   }
 
