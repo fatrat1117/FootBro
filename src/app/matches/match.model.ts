414a221c44f082c72f9e77209a6ee2376d2189a0
fatrat1117 <fatrat1117@gmail.com>
2017-02-19 10:43:58 +0800

    show all matches of a team

--- src/app/matches/match.model.ts
+++ src/app/matches/match.model.ts
@@ -1,25 +1,7 @@
 import { Team} from '../teams/team.model';
 
-export class MatchBasic {
-  time: number;
-  homeId: string;
-  awayId: string;
-  homeScore: number;
-  awayScore: number;
-}
-
-export class MatchStanding {
-  teamId: string;
-  matchesPlayed: number;
-  wins: number;
-  draws: number;
-  losses: number;
-  goalsFor: number;
-  goalsAgainst: number;
-  points: number;
-}
-
 export class Match {
+  $key;
   id;
   date;
   time;
