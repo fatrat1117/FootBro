9d2e74a6a641046c261a4e50015f9786e12044c7
ray-z <710956@gmail.com>
2017-03-02 22:16:47 +0800

    Allow no title for rate circle.

--- src/app/common/rate.circle.component.ts
+++ src/app/common/rate.circle.component.ts
@@ -5,9 +5,10 @@ import { Component, Input } from '@angular/core';
   template: `
     <div class="round-progress-container">
       <round-progress [current]="rate" [max]=100 [color]="'#'+'00ff00'" [background]="'#'+'a0a0a0'" [radius]="125"
-        [stroke]="20" [semicircle]="false" [rounded]="true" [clockwise]="false" [responsive]="true"> </round-progress>
+        [stroke]="20" [semicircle]="false" [rounded]="true" [clockwise]="false" [responsive]="true">
+      </round-progress>
       <span class="circle-text">
-        <div class="win-ratio-label">{{ title | trans }}</div>
+        <div *ngIf="title" class="win-ratio-label">{{ title | trans }}</div>
         <div class="win-ratio">{{ rate }}%</div>
       </span>
     </div>
