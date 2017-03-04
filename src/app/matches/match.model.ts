c9402d64dd02fc3d79986bc952c00f71bff4662d
fatrat1117 <fatrat1117@gmail.com>
2017-03-04 11:36:15 +0800

    update match state

--- src/app/matches/match.model.ts
+++ src/app/matches/match.model.ts
@@ -25,9 +25,24 @@ export class Match {
   createBy;
   homeParticipants = [];
   awayParticipants = [];
+  isHomeUpdated;
+  isAwayUpdated;
   isStarted = function() {
     let now = moment().unix() * 1000;
     //console.log(now, this.time);
     return now > this.time;
+  };
+  updateState = function () {
+    //0: no one update
+    //1: home updated
+    //2: away updated
+    //3 both updated
+    if (this.isHomeUpdated && this.isAwayUpdated)
+      return 3;
+    if (this.isHomeUpdated)
+      return 1;
+    if (this.isAwayUpdated)
+      return 2;
+    return 0;
   }
 }
