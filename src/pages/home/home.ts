175849b6edf91827eab76a05b41149d900ee2c05
ray-z <710956@gmail.com>
2017-03-26 21:37:28 +0800

    Changed home page style (WIP)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,21 +8,59 @@ import {ModalContentPage} from "../home/place-selection";
 import {GameRatingPage} from "../game-rating/game-rating";
 import { MatchesPage } from '../matches/matches';
 
+import { Player } from '../../app/players/player.model'
+import { PlayerService } from '../../app/players/player.service'
+import { Team } from '../../app/teams/team.model'
+import { TeamService } from '../../app/teams/team.service'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
   slides: any[];
   items = ["item1","item2","item3"];
+  selfPlayer: Player;
+  selfTeam: Team;
+  onPlayerReady;
+  onTeamReady;
 
-  constructor(public navCtrl: NavController, local: Localization,private elRef: ElementRef,public modalCtrl: ModalController  ) {
+  constructor(public navCtrl: NavController, local: Localization,private elRef: ElementRef, public modalCtrl: ModalController, 
+              private playerService: PlayerService, private teamService: TeamService) {
     this.slides = [];
     //this.loadSlides(local.langCode, 4);
     this.loadSlides(local.langCode, 1);
+    this.selfPlayer = null;
+    this.selfTeam = null;
   }
 
-  ionViewDidLoad() :any{
+  ionViewDidLoad() {
+    document.addEventListener('userlogin', e => {
+      this.playerService.getPlayerAsync(this.playerService.selfId());
+      document.addEventListener('serviceplayerready', this.onPlayerReady);
+    });
+
+    document.addEventListener('userlogout', e => {
+      this.selfPlayer = null;
+      this.selfTeam = null
+      document.removeEventListener('serviceteamready', this.onTeamReady);
+      document.removeEventListener('serviceplayerready', this.onPlayerReady);
+    });
+
+    this.onPlayerReady = e => {
+      if (this.playerService.selfId() && e['detail'] == this.playerService.selfId()) {
+        this.selfPlayer = this.playerService.getPlayer(e['detail']);
+        this.teamService.getTeamAsync(this.selfPlayer.teamId);
+        document.addEventListener('serviceteamready', this.onTeamReady);
+      }
+    };
+
+    this.onTeamReady = e => {
+      if (this.selfPlayer.teamId && e['detail'] == this.selfPlayer.teamId) {
+        this.selfTeam = this.teamService.getTeam(e['detail']);
+      }
+    }
   }
 
   loadSlides(langCode: string, total: number) {
