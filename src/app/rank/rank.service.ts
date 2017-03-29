35fbf19b99a519fe4a7e5a7fc4327fbc8b38d58c
Howie <hao.li@razerzone.com>
2017-03-29 18:49:05 +0800

    optimize rank

--- src/app/rank/rank.service.ts
+++ src/app/rank/rank.service.ts
@@ -10,76 +10,77 @@ import { Team } from '../../app/teams/team.model';
 export class RankService {
   playerRanks = [];
   teamRanks = [];
-  needRefreshTeamsRankUI = false;
-  playerRanksMap = {};
-  teamRanksMap = {};
+  //needRefreshTeamsRankUI = false;
+  //playerRanksMap = {};
+  //teamRanksMap = {};
   constructor(private fm: FirebaseManager,
     private playerService: PlayerService,
     private teamService: TeamService) {
     document.addEventListener('publicteamschanged',
       e => {
-          this.needRefreshTeamsRankUI = false;
+          //this.needRefreshTeamsRankUI = false;
           this.teamRanks = [];
           let sortedPublicTeams = this.fm.sortedPublicTeams;
           for (let i = 0; i < sortedPublicTeams.length; ++i) {
             let teamId = sortedPublicTeams[i].$key;
-            let teamRankData = {
-              id: teamId,
-              name: sortedPublicTeams[i].name,
-              logo: "assetsimg/none.png",
-              totalPlayers: 0,
-              ability: sortedPublicTeams[i].ability,
-              popularity: sortedPublicTeams[i].popularity
-            };
+            let teamRankData = this.teamService.findOrCreateTeamAndPull(teamId);
+            // let teamRankData = {
+            //   id: teamId,
+            //   name: sortedPublicTeams[i].name,
+            //   logo: "assetsimg/none.png",
+            //   totalPlayers: 0,
+            //   ability: sortedPublicTeams[i].ability,
+            //   popularity: sortedPublicTeams[i].popularity
+            // };
             this.teamRanks.push(teamRankData);
             //easy find taemRankData
-            this.teamRanksMap[teamId] = teamRankData;
-            this.teamService.getTeamAsync(teamId);
+            //this.teamRanksMap[teamId] = teamRankData;
+            //this.teamService.getTeamAsync(teamId);
           }
           this.fm.FireEvent('serviceteamrankschanged');
         }
       );
 
-    document.addEventListener('serviceteamready', e => {
-      let teamId = e['detail'];
-      let team = this.teamService.getTeam(teamId);
-      let teamRankData = this.teamRanksMap[teamId];
-      if (team && teamRankData) {
-        teamRankData.logo = team.logo;
-        teamRankData.totalPlayers = team.totalPlayers;
-        teamRankData.ability = team.ability;
-        teamRankData.popularity = team.popularity;
-      }
-    }
-    );
+    // document.addEventListener('serviceteamready', e => {
+    //   let teamId = e['detail'];
+    //   let team = this.teamService.getTeam(teamId);
+    //   let teamRankData = this.teamRanksMap[teamId];
+    //   if (team && teamRankData) {
+    //     teamRankData.logo = team.logo;
+    //     teamRankData.totalPlayers = team.totalPlayers;
+    //     teamRankData.ability = team.ability;
+    //     teamRankData.popularity = team.popularity;
+    //   }
+    // }
+    // );
 
     document.addEventListener('publicplayerschanged', e => {
       let sortedPublicPlayers = this.fm.sortedPublicPlayers;
       this.playerRanks = [];
       for (let i = 0; i < sortedPublicPlayers.length; ++i) {
-        let playerRank = new PlayerRank();
-        let id = sortedPublicPlayers[i].$key;
-        playerRank.popularity = sortedPublicPlayers[i].popularity;
-        playerRank.id = id;
+        let playerId = sortedPublicPlayers[i].$key;
+        let playerRank = this.playerService.findOrCreatePlayerAndPull(playerId);
+        // playerRank.popularity = sortedPublicPlayers[i].popularity;
+        // playerRank.id = id;
         this.playerRanks.push(playerRank);
-        this.playerRanksMap[id] = playerRank;
-        this.playerService.getPlayerAsync(id);
+        // this.playerRanksMap[id] = playerRank;
+        // this.playerService.getPlayerAsync(id);
       }
 
       this.fm.FireEvent('serviceplayerrankschanged');
     });
 
-    document.addEventListener('serviceplayerready', e => {
-      let id = e['detail'];
-      let player = this.playerService.getPlayer(id);
-      let playerRank = this.playerRanksMap[id];
-      if (playerRank) {
-        playerRank.photo = player.photo;
-        playerRank.name = player.name;
-        playerRank.position = player.position;
-        playerRank.popularity = player.popularity;
-      } 
-    });
+    // document.addEventListener('serviceplayerready', e => {
+    //   let id = e['detail'];
+    //   let player = this.playerService.getPlayer(id);
+    //   let playerRank = this.playerRanksMap[id];
+    //   if (playerRank) {
+    //     playerRank.photo = player.photo;
+    //     playerRank.name = player.name;
+    //     playerRank.position = player.position;
+    //     playerRank.popularity = player.popularity;
+    //   } 
+    // });
   }
 
   getTeamRanksAsync(orderby, count) {
