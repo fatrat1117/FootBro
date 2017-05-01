20986a50a8ce9e5f0ebf1cbeaa363b253f767944
fatrat1117 <fatrat1117@gmail.com>
2017-05-01 15:41:37 +0800

    display group

--- src/app/matches/match.service.ts
+++ src/app/matches/match.service.ts
@@ -35,8 +35,7 @@ export class MatchService {
       match.isAwayUpdated = fmMatch.isAwayUpdated;
       match.dataReady = true;
       match.groupId = fmMatch.groupId;
-      console.log(match.groupId);
-      
+      //console.log(match.groupId);
       match.home = this.teamService.findOrCreateTeam(match.homeId);
       this.fm.getTeamAsync(match.homeId);
       match.away = this.teamService.findOrCreateTeam(match.awayId);
