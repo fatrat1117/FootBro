90f32637da58303175160b8e1789bed89b4ca359
fatrat1117 <fatrat1117@gmail.com>
2017-07-30 09:46:15 +0800

    video urls

--- src/pages/training/training.ts
+++ src/pages/training/training.ts
@@ -6,6 +6,28 @@ import { NavParams, ModalController, ViewController} from 'ionic-angular'
   templateUrl: 'training.html'
 })
 export class TrainingPage {
+  videos = [];
   constructor() {
+    this.videos = [{
+      title: 'abc',
+      level: 1,
+      description: 'xxxx',
+      url: 'https://www.youtube.com/embed/Gym1QEsdHI0',
+      type: 'physical'
+    },
+    {
+      title: 'fdsfds',
+      level: 2,
+      description: 'xxxx',
+      url: 'https://www.youtube.com/embed/Gym1QEsdHI0',
+      type: 'technique'
+    },
+    {
+      title: 'gdgfdg',
+      level: 3,
+      description: 'xxxx',
+      url: 'https://www.youtube.com/embed/Gym1QEsdHI0',
+      type: 'tactics'
+    }];
   }
 }
